59aa45bade7af487643a94989c3986817b041ac5
Joaquim Kalala <joaquim.kalala@gmail.com>
2016-02-21 19:47:12 +0100

    table usuarios removed

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 21-Fev-2016 às 17:48
+-- Generation Time: 21-Fev-2016 às 19:42
 -- Versão do servidor: 10.1.9-MariaDB
 -- PHP Version: 7.0.1
 
@@ -96,28 +96,9 @@ INSERT INTO `registro` (`ID`, `name`, `email`, `username`, `password`, `password
 (5, 'Bentley Brown', 'brentley@gmail.com', 'b.brown', '8cb2237d0679ca88db6464eac60da96345513964', '8cb2237d0679ca88db6464eac60da96345513964'),
 (6, 'Fernado Torres', 'f.torres@yahoo.fr', 'f.torres', '8cb2237d0679ca88db6464eac60da96345513964', '8cb2237d0679ca88db6464eac60da96345513964'),
 (7, 'Pedro SebastiÃ£o', 'joaquim.kalala@gmail.com', 'pedro20', '7c4a8d09ca3762af61e59520943dc26494f8941b', '7c4a8d09ca3762af61e59520943dc26494f8941b'),
-(8, 'Paulo Gomez', 'gomes.paulo@gmail.com', 'g.paulo', '987e324fc327c7cc74841f88ff5a24a3233c7e41', '987e324fc327c7cc74841f88ff5a24a3233c7e41');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `hashed_password` varchar(100) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `usuario`, `hashed_password`) VALUES
-(1, 'kalala', '2430737b3f12632d49f5f1d79b88fb27adf15214'),
-(2, 'peterson', '50f139767ff9e7a2b155604646893e93f3a92aea'),
-(8, 'Jonathan', '2d070b0f8acfa49e032a30af6e58d9d7fc61067b');
+(8, 'Paulo Gomez', 'gomes.paulo@gmail.com', 'g.paulo', '987e324fc327c7cc74841f88ff5a24a3233c7e41', '987e324fc327c7cc74841f88ff5a24a3233c7e41'),
+(9, 'barack obama', 'obama@state.gov', 'barack', 'f64217920611283e807cd2dd8a311c3303d8632c', 'f64217920611283e807cd2dd8a311c3303d8632c'),
+(10, 'Joaquim Kangomba Kalala', 'joaquim.kalala@gmail.com', 'jkalala', '2430737b3f12632d49f5f1d79b88fb27adf15214', '2430737b3f12632d49f5f1d79b88fb27adf15214');
 
 --
 -- Indexes for dumped tables
@@ -142,12 +123,6 @@ ALTER TABLE `registro`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -160,12 +135,7 @@ ALTER TABLE `cadastro`
 -- AUTO_INCREMENT for table `registro`
 --
 ALTER TABLE `registro`
-  MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
